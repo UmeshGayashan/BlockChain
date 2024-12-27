8f5b93fa91a0c6929192e534eca3499b1f908ff8
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2024-12-27 18:10:33 +0530

    create new Transaction

--- dev/blockChain.js
+++ dev/blockChain.js
@@ -1,22 +1,38 @@
 function BlockChain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 BlockChain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
+}
+
+BlockChain.prototype.getLastBlock = function() {
+    return this.chain[this.chain.length - 1];
+}
+
+BlockChain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
 }
 
 module.exports = BlockChain;
--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,8 @@ const BlockChain  = require('./blockChain');
 
 const dummyCoin = new BlockChain();
 dummyCoin.createNewBlock(1234, '0', 'HGBOUYGBUO');
-dummyCoin.createNewBlock(1234, 'HGBOUYGBUO', 'HGBHUOOUYGBUO');
+dummyCoin.createNewTransaction(100, 'ALEXHJG', 'JENNYHJG');
+dummyCoin.createNewBlock(124, 'HGBOUYGBUO', 'HGBOUYGBUOHIIG');
 
 console.log(dummyCoin);
+console.log(dummyCoin.chain[1]);
