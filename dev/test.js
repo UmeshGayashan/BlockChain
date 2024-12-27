02642595749cd21cf8367ceb44e663544821be2d
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2024-12-28 00:51:56 +0530

    testing proof of work

--- dev/test.js
+++ dev/test.js
@@ -1,9 +1,33 @@
 const BlockChain  = require('./blockChain');
 
 const dummyCoin = new BlockChain();
-dummyCoin.createNewBlock(1234, '0', 'HGBOUYGBUO');
-dummyCoin.createNewTransaction(100, 'ALEXHJG', 'JENNYHJG');
-dummyCoin.createNewBlock(124, 'HGBOUYGBUO', 'HGBOUYGBUOHIIG');
+// dummyCoin.createNewBlock(1234, '0', 'HGBOUYGBUO');
+// dummyCoin.createNewTransaction(100, 'ALEXHJG', 'JENNYHJG');
+// dummyCoin.createNewBlock(124, 'HGBOUYGBUO', 'HGBOUYGBUOHIIG');
+
+const previousBlockHash = 'HGBOUYGBUO';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'ALEXHJG',
+        recipient: 'JENNYHJG'
+    },
+    {
+        amount: 30,
+        sender: 'JENNYHJG',
+        recipient: 'ALEXHJG'
+    },
+    {
+        amount: 200,
+        sender: 'ALEXHJG',
+        recipient: 'JENNYHJG'
+    }
+];
+
+const nonce = dummyCoin.proofOfWork(previousBlockHash, currentBlockData);
+const hash = dummyCoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+console.log(`Hash: ${hash} and nonce: ${nonce}`);
+
 
 console.log(dummyCoin);
 console.log(dummyCoin.chain[1]);
