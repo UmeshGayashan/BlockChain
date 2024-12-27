1d341b1ec15f7756c60293f7cdc48683c6907e5c
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2024-12-28 01:01:14 +0530

    setup for api calls

--- dev/test.js
+++ dev/test.js
@@ -5,29 +5,29 @@ const dummyCoin = new BlockChain();
 // dummyCoin.createNewTransaction(100, 'ALEXHJG', 'JENNYHJG');
 // dummyCoin.createNewBlock(124, 'HGBOUYGBUO', 'HGBOUYGBUOHIIG');
 
-const previousBlockHash = 'HGBOUYGBUO';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'ALEXHJG',
-        recipient: 'JENNYHJG'
-    },
-    {
-        amount: 30,
-        sender: 'JENNYHJG',
-        recipient: 'ALEXHJG'
-    },
-    {
-        amount: 200,
-        sender: 'ALEXHJG',
-        recipient: 'JENNYHJG'
-    }
-];
+// const previousBlockHash = 'HGBOUYGBUO';
+// const currentBlockData = [
+//     {
+//         amount: 10,
+//         sender: 'ALEXHJG',
+//         recipient: 'JENNYHJG'
+//     },
+//     {
+//         amount: 30,
+//         sender: 'JENNYHJG',
+//         recipient: 'ALEXHJG'
+//     },
+//     {
+//         amount: 200,
+//         sender: 'ALEXHJG',
+//         recipient: 'JENNYHJG'
+//     }
+// ];
 
-const nonce = dummyCoin.proofOfWork(previousBlockHash, currentBlockData);
-const hash = dummyCoin.hashBlock(previousBlockHash, currentBlockData, nonce);
-console.log(`Hash: ${hash} and nonce: ${nonce}`);
+// const nonce = dummyCoin.proofOfWork(previousBlockHash, currentBlockData);
+// const hash = dummyCoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+// console.log(`Hash: ${hash} and nonce: ${nonce}`);
 
 
 console.log(dummyCoin);
-console.log(dummyCoin.chain[1]);
+// console.log(dummyCoin.chain[1]);
