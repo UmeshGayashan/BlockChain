749e3243e5a4971607ba41464ba4acf16c7a99da
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2024-12-29 10:45:40 +0530

    include body-parser

--- dev/api.js
+++ dev/api.js
@@ -1,5 +1,9 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World')
