34dfd79947d6a66deb95da11e3b1f37912b8120b
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2024-12-29 20:00:44 +0530

    mine api and uuid package

--- dev/api.js
+++ dev/api.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const BlockChain  = require('./blockChain');
+
+const dummyCoin = new BlockChain();
 
 const app = express()
 app.use(bodyParser.json());
@@ -10,15 +13,30 @@ app.get('/', function (req, res) {
 })
 
 app.get('/blockchain', function (req, res) {
-
+  res.send(dummyCoin);
 })
 
 app.post('/transaction', function (req, res) {
-
+  const blockIndex = dummyCoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
 })
 
 app.get('/mine', function (req, res) {
+  const lastBlock = dummyCoin.getLastBlock();
+  const previousBlockHash = lastBlock['hash'];
+  const currentBlockData = {
+    transactions: dummyCoin.pendingTransactions,
+    index: lastBlock['index'] + 1
+  };
 
+  const nonce = dummyCoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = dummyCoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+  const newBlock = dummyCoin.createNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({
+    note: 'New block mined successfully',
+    block: newBlock
+  });
 })
 
 app.listen(3000, function () {
