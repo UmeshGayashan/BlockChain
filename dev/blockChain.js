9a3eca9b37b20100f1ec4615220fd1ea5ba9e710
H.L.D.U.G Perera <umeshgayashan099@gmail.com>
2025-01-30 10:55:52 +0530

    Split in to 2 part createNewTransaction Methods

--- dev/blockChain.js
+++ dev/blockChain.js
@@ -1,5 +1,6 @@
 const sha256 = require('sha256');
 const currentNodeUrl = process.argv[3];
+const { v1: uuidv1 } = require('uuid');
 
 function BlockChain() {
     this.chain = [];
@@ -36,12 +37,16 @@ BlockChain.prototype.createNewTransaction = function(amount, sender, recipient) 
     const newTransaction = {
         amount: amount,
         sender: sender,
-        recipient: recipient
+        recipient: recipient,
+        transactionId: uuidv1().split('-').join('')
     };
 
-    this.pendingTransactions.push(newTransaction);
+    return newTransaction;
+}
 
-    return this.getLastBlock()['index'] + 1; // New Transaction will be added to the next(This) block
+BlockChain.prototype.addTransactionToPendingTransaction = function(transactionObj){
+    this.pendingTransactions.push(transactionObj);
+    return this.getLastBlock()['index'] = 1;
 }
 
 BlockChain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
