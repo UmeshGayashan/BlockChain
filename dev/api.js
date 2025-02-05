eebe0092c6b63c2cb3767b57c8c77c68fbb6829d
H.L.D.U.G Perera <umeshgayashan099@gmail.com>
2025-02-05 23:40:54 +0530

    broadcast transaction to all the network nodes in the network

--- dev/api.js
+++ dev/api.js
@@ -27,7 +27,25 @@ app.post('/transaction', function (req, res) {
 })
 
 app.post('/transaction/broadcast', function(req,res){
-  const newTransaction = dummyCoin.createNewTransaction
+  const newTransaction = dummyCoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  dummyCoin.addTransactionToPendingTransaction(newTransaction);
+
+  const requestPromises = [];
+  dummyCoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/transaction',
+      method: 'POST',
+      body: newTransaction,
+      json: true
+    };
+
+    requestPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(requestPromises)
+    .then(data => {
+      res.json({ note: 'Transaction created and broadcast successfully.' });
+    });
 })
 
 app.get('/mine', function (req, res) {
